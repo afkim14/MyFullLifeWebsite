0efd06e85c58a86bcfd8b1a8007b27aa7a0b1203
Andres Kim <andreskim2018@u.northwestern.edu>
2019-02-19 19:29:28 -0600

    Add files via upload

---
+++ precache-manifest.b7ddb939555ca4aca38e68e4b1104e2c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/MyFullLife/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b658e452294316e71616",
+    "url": "/MyFullLife/static/js/runtime~main.b658e452.js"
+  },
+  {
+    "revision": "78119a69a40582d97003",
+    "url": "/MyFullLife/static/js/main.78119a69.chunk.js"
+  },
+  {
+    "revision": "8dbfe12b039dd09afdf3",
+    "url": "/MyFullLife/static/js/2.8dbfe12b.chunk.js"
+  },
+  {
+    "revision": "78119a69a40582d97003",
+    "url": "/MyFullLife/static/css/main.44deac7f.chunk.css"
+  },
+  {
+    "revision": "8dbfe12b039dd09afdf3",
+    "url": "/MyFullLife/static/css/2.ea3c29df.chunk.css"
+  },
+  {
+    "revision": "a24338dee336272f5ccbe1514472d7d3",
+    "url": "/MyFullLife/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/MyFullLife/precache-manifest.b7ddb939555ca4aca38e68e4b1104e2c.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/MyFullLife/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
